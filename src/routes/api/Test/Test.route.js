132bebbd92894124e752a659792db5c223ae4e5f
amanbadyal7214 <149406745+amanbadyal7214@users.noreply.github.com>
2026-08-20 23:44:42 +0530

    feat: implement API routes for test management, question CRUD operations, and event handling

--- src/routes/api/Test/Test.route.js
+++ src/routes/api/Test/Test.route.js
@@ -6,7 +6,7 @@ const fetchQuestion = require("../../../../controllers/Test/fetchQuestion");
 const submitAnswer = require("../../../../controllers/Test/submitAnswer");
 const submitTest = require("../../../../controllers/Test/submitTest");
 const addQuestion = require("../../../../controllers/Test/addQuestion");
-const { getTestResults, toggleShortlist, getShortlistedStudents } = require("../../../../controllers/Test/getTestResults");
+
 const { createEvent, getEvents, toggleEventStatus } = require("../../../../controllers/Test/createEvent");
 const { getAllQuestions, updateQuestion, deleteQuestion } = require("../../../../controllers/Test/manageQuestions");
 
@@ -34,9 +34,6 @@ router.post("/answer", submitAnswer);
 // POST /api/test/submit (Student submits test)
 router.post("/submit", submitTest);
 
-// GET /api/test/results & POST /api/test/toggle-shortlist
-router.get("/results", getTestResults);
-router.get("/shortlisted", getShortlistedStudents);
-router.post("/toggle-shortlist", toggleShortlist);
+
 
 module.exports = router;
